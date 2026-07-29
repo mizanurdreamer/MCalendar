21b73df6e8c9424123bca2b47f9ecb030fcd53fd
Mizanur Rahaman <mizanurrahamandu@gmail.com>
2026-07-29 09:13:37 +0600

    eslint removed

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,6 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
   output: "standalone",
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   experimental: {
     serverActions: {
       bodySizeLimit: "2mb",
